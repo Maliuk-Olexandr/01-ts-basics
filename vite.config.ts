af989b79cc7d894208ccdd913bfa93ff5223b813
Maliuk-Olexandr <maamcd100@gmail.com>
2025-07-17 00:59:58 +0300

    refactor: remove index.html, update package.json and package-lock.json with new dependencies, and modify tsconfig.json
    - Deleted index.html file.
    - Updated package.json to include vite-plugin-full-reload and vite-plugin-html-inject.
    - Updated package-lock.json to reflect new dependencies and their versions.
    - Added @types/node, glob, postcss, and postcss-sort-media-queries to devDependencies in package.json.
    - Modified tsconfig.json to ensure "types": ["node"] is included.
    - Created a new vite.config.ts file with configuration for Vite, including plugins for HTML injection and full reload.

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from "vite";
+import { globSync } from "glob";
+import path from "path";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import SortCss from "postcss-sort-media-queries";
+import type { UserConfig } from "vite";
+
+export default defineConfig(({ command }): UserConfig => {
+  const htmlFiles = globSync("./src/*.html");
+
+  const input: Record<string, string> = htmlFiles.reduce((acc, file) => {
+    const name = path.basename(file, path.extname(file));
+    acc[name] = path.resolve(file);
+    return acc;
+  }, {} as Record<string, string>);
+
+  return {
+    root: "src",
+    //base: "/01-ts-basics/", // ← дуже важливо для GitHub Pages
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
+    },
+    build: {
+      target: "esnext",
+      sourcemap: true,
+      outDir: "../dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input,
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
+            }
+            return "[name].js";
+          },
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
+            }
+            return "assets/[name]-[hash][extname]";
+          },
+        },
+      },
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(["./src/**/*.html"]),
+      SortCss({
+        sort: "mobile-first",
+      }),
+    ],
+  };
+});
